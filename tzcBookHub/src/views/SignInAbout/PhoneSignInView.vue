20e7f48fad5663db695100a24df631cdfcbfd9cf
Kettkk <a962498793@gmail.com>
2024-09-04 10:45:20 +0800

    password and phoneNumber Inputer

--- tzcBookHub/src/views/SignInAbout/PhoneSignInView.vue
+++ tzcBookHub/src/views/SignInAbout/PhoneSignInView.vue
@@ -1,53 +1,107 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
-const phoneNumber = ref('');
-// 动态绑定样式
-const inputStyles = {
-  color: 'blue', // 输入文本的颜色
-  backgroundColor: 'rgb(251,239,218)', // 输入框背景色
-  borderRadius: '8px', // 圆角
-};
 
+const phoneNumber = ref('');
+const password = ref('');
+const showPassword = ref(false);
+
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
+};
 </script>
 
 <template>
-<div>
-  <div id="textContainer">
-    <div id="Title1">开启刷题之旅</div>
-    <div id="EnTitle">Sign in</div>
-  </div>
+  <div>
+    <div id="textContainer">
+      <div id="Title1">开启刷题之旅</div>
+      <div id="EnTitle">Sign in</div>
+    </div>
 
-  <div id="FormContainer">
-    <div id="inputer">
-      <el-input
-          v-model="phoneNumber"
-          placeholder="手机号"
-          :style="inputStyles"
-      />
+    <div id="FormContainer">
+      <div id="inputer">
+        <input
+            type="text"
+            placeholder="手机号"
+            v-model="phoneNumber"
+            id="customInput"
+        />
+        <div id="passwordContainer">
+          <input
+              :type="showPassword ? 'text' : 'password'"
+              placeholder="密码"
+              v-model="password"
+              id="customInput"
+          />
+          <button type="button" id="toggleButton" @click="togglePasswordVisibility">
+            <svg
+                v-if="sh"
+                t="1725417413708"
+                class="icon"
+                viewBox="0 0 1024 1024"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                p-id="4879"
+                data-spm-anchor-id="a313x.search_index.0.i10.65d43a81RV3wjL" width="200" height="200">
+              <path d="M512 256a416.853333 416.853333 0 0 1 376.32 234.666667 411.605333 411.605333 0 0 1-102.826667 133.12l60.16 60.16c59.306667-52.48 106.24-118.186667 135.68-193.28C907.52 303.36 725.333333 170.666667 512 170.666667c-54.186667 0-106.24 8.533333-155.306667 24.32l70.4 70.4C454.826667 259.84 482.986667 256 512 256z m-45.653333 48.64L554.666667 392.96c24.32 10.666667 43.946667 30.293333 54.613333 54.613333l88.32 88.32c3.413333-14.506667 5.973333-29.866667 5.973333-45.653333C704 384.426667 617.813333 298.666667 512 298.666667c-15.786667 0-30.72 2.133333-45.653333 5.973333zM85.76 165.12l114.346667 114.346667A500.821333 500.821333 0 0 0 42.666667 490.666667C116.48 677.973333 298.666667 810.666667 512 810.666667c64.853333 0 127.146667-12.373333 184.32-34.986667l145.92 145.92 60.16-60.16L145.92 104.533333 85.76 165.12z m320 320l111.36 111.36c-1.706667 0.426667-3.413333 0.853333-5.12 0.853333a106.666667 106.666667 0 0 1-106.666667-106.666666c0-2.133333 0.426667-3.413333 0.426667-5.546667z m-145.066667-145.066667l74.666667 74.666667a196.266667 196.266667 0 0 0-15.36 75.946667 192.298667 192.298667 0 0 0 267.52 176.64l41.813333 41.813333c-37.546667 10.24-76.8 16.213333-117.333333 16.213333a416.853333 416.853333 0 0 1-376.32-234.666666c29.866667-61.013333 73.386667-111.36 125.013333-150.613334z" p-id="4880" fill="#AE782A"></path>
+            </svg>
+
+          </button>
+        </div>
+      </div>
     </div>
   </div>
-
-</div>
 </template>
 
 <style scoped>
-#textContainer{}
-#Title1{
+#textContainer {}
+#Title1 {
   font-size: 23px;
   font-weight: bold;
   margin-top: 45%;
   margin-left: 5%;
 }
-#EnTitle{
+#EnTitle {
   font-size: 15px;
   font-weight: bold;
   color: rgb(233,168,69);
   margin-left: 5%;
 }
-
-/* 自定义占位符的颜色 */
-.el-input__inner::placeholder {
-  color: black; /* 占位符文字颜色 */
+#inputer {
+  margin-left: 5%;
+  margin-top: 5%;
 }
 
+#customInput {
+  width: 90%;
+  height: 38px;
+  padding: 10px;
+  border-radius: 8px;
+  background-color: rgb(251,239,218); /* 背景色 */
+  border: 1px solid transparent; /* 去除边框 */
+  font-size: 16px; /* 字体大小 */
+  color: rgb(222, 160, 66); /* 输入的字体颜色 */
+  outline: none; /* 去掉点击时的外边框 */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 5%;
+}
+
+/* 自定义占位符的颜色 */
+#customInput::placeholder {
+  color: rgb(224, 168, 83); /* 占位符文字颜色 */
+}
+
+#passwordContainer {
+  position: relative;
+  width: 90%;
+}
+
+#toggleButton {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  cursor: pointer;
+}
 </style>
